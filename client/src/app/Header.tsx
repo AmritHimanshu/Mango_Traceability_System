2a61be2701022854ec638a583a7ec91f233fd507
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-04 23:35:57 +0530

    Creating header

--- client/src/app/Header.tsx
+++ client/src/app/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import Image from "next/image";
 import mango_logo from "../../public/Mango_logo.png";
@@ -9,7 +11,7 @@ function Header() {
   return (
     <div className="py-2 px-4 bg-green-100 flex items-center justify-between">
       <div className="flex items-center justify-start">
-        <Image src={mango_logo} alt="mango_logo" height={40} width={40}></Image>
+        <Image src={mango_logo} alt="mango_logo" priority={true} height={40} width={40}></Image>
         <div className="flex flex-col items-center">
           <p className="text-xl font-bold">MTS</p>
           <div className="text-[5px] font-bold">Mango Traceability System</div>
