df838b8e3402fe02ddeaac44e45471d006f56975
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-05 23:36:39 +0530

    Creating header

--- client/src/app/Header_Menu.js
+++ client/src/app/Header_Menu.js
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation';
 
 function Header_Menu({onNavigationComplete}) {
 
