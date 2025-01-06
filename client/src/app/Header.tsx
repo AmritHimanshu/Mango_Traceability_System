436bd4d1d9f95cd482de49580ab3a80a896a807d
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-06 20:51:35 +0530

    Updated Header_Menu

--- client/src/app/Header.tsx
+++ client/src/app/Header.tsx
@@ -8,33 +8,35 @@ import Header_Menu from "./Header_Menu";
 // Material UI Icon
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import Link from "next/link";
 
 function Header() {
-
   const [isMenu, setIsMenu] = useState(false);
-  
-  const handleIsMenuState = ()=>{
+
+  const handleIsMenuState = () => {
     setIsMenu(false);
-  }
+  };
 
   return (
     <>
       <div className="py-2 px-4 bg-green-100 flex items-center justify-between relative">
-        <div className="flex items-center justify-start">
-          <Image
-            src={mango_logo}
-            alt="mango_logo"
-            priority={true}
-            height={40}
-            width={40}
-          />
-          <div className="flex flex-col items-center">
-            <p className="text-xl font-bold">MTS</p>
-            <div className="text-[5px] font-bold">
-              Mango Traceability System
+        <Link href="/">
+          <div className="flex items-center justify-start">
+            <Image
+              src={mango_logo}
+              alt="mango_logo"
+              priority={true}
+              height={40}
+              width={40}
+            />
+            <div className="flex flex-col items-center">
+              <p className="text-xl font-bold">MTS</p>
+              <div className="text-[5px] font-bold">
+                Mango Traceability System
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
 
         {!isMenu ? (
           <MenuIcon onClick={() => setIsMenu(!isMenu)} />
@@ -44,7 +46,7 @@ function Header() {
       </div>
       {isMenu && (
         <div>
-          <Header_Menu onNavigationComplete={handleIsMenuState}/>
+          <Header_Menu onNavigationComplete={handleIsMenuState} />
         </div>
       )}
     </>
