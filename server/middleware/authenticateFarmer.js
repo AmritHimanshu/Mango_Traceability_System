e48138643d226714008cf6dd6a17efc71833d027
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-10 00:03:40 +0530

    Creating authentication middleware

---
+++ server/middleware/authenticateFarmer.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/userSchema');
+
+const authenticateFarmer = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwtoken;
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+
+        if (!verifyToken) {
+            return res.status(401).json({ error: "Token has expired" });
+        }
+
+        const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
+        if (!rootUser) {
+            return res.status(401).json({ error: "User not found" });
+        }
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: "Unauthorized: Token has expired" });
+        } else {
+            return res.status(401).json({ error: "Unauthorized: No valid token provided" });
+        }
+    }
+}
+
+module.exports = authenticateFarmer;
