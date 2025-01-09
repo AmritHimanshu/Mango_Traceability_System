25b68e63da9c194f0334909d7009e4ed35d3e607
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-09 18:44:09 +0530

    Added password hash and token generation function

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema(
     {
@@ -38,11 +40,39 @@ const userSchema = new mongoose.Schema(
             default: false
 
         },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                }
+            }
+        ],
     },
     {
         timestamps: true
     }
 );
+
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let Token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({ token: Token });
+        await this.save();
+        return Token;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 const User = mongoose.model('USER', userSchema);
 
