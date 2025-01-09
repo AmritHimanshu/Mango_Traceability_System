df05835866754558b2229591d97346f82d57792d
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-09 17:02:17 +0530

    Building server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const PORT = process.env.PORT || 5000;
+
+require('./db/conn');
+
+app.use(cors({
+    origin: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`The server is running at port ${PORT}`);
+});
