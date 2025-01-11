e613dcd839931f5459972a9c1d5699b3f6f315d7
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-11 12:07:32 +0530

    Adding redux-persist for data persistence after refresh

--- client/src/store/redux-provider.tsx
+++ client/src/store/redux-provider.tsx
@@ -2,7 +2,8 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store";
-// import { persistStore } from "redux-persist";
+import { persistStore } from "redux-persist";
+persistStore(store);
 
 export default function ReduxProvider({
   children,
