a20f88b758b5d6d140a270cd36a5609ee2ea8f4e
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-14 00:28:52 +0530

    Creating Farmer user routes

---
+++ client/src/app/components/farmer/Home.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      This is farmer home page
+      <div></div>
+    </div>
+  );
+}
+
+export default Home;
--- client/src/utils/Paths/paths.tsx
+++ client/src/utils/Paths/paths.tsx
@@ -1,9 +1,15 @@
 // Role = ADMIN
-export const OVERVIEW = "/";
 export const MANAGER_MANAGEMENT = "/admin/manager-management";
 export const FARMER_MANAGEMENT = "/admin/farmer-management";
 export const PENDING_REQUESTS = "/admin/pending-requests";
 
+// Farmers
+export const FARMS = "/farmer/farms";
+export const NOTIFICATIONS = "/farmer/notifications";
+export const PROFILE = "/farmer/profile";
+
+// Common
+export const OVERVIEW = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
 export const LOGOUT = "/logout";
