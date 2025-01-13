dd87380767b1ca09e813ab2206d489e2518a9519
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-13 10:37:50 +0530

    Added logout functionality

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,17 +2,22 @@
 
 import { useEffect, useState } from "react";
 import { useAppSelector } from "@/store/store";
+import { useRouter } from "next/navigation";
+import { LOGIN } from "@/utils/Paths/paths";
 import AdminHome from './components/admin/Home';
 import "../styles/style.css";
 
 export default function Home() {
 
   const userState = useAppSelector((state) => state.user.userState);
+  
+  const router = useRouter();
 
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
+    if(!userState) return router.push(LOGIN);
   }, []);
 
   if (!isClient) return null;
