4937b7c7858f8d1640ff4f02f47b4669e3798a31
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-13 16:13:03 +0530

    Added truncate propertiy

--- client/src/app/components/admin/components/ListUserCard.tsx
+++ client/src/app/components/admin/components/ListUserCard.tsx
@@ -9,9 +9,10 @@ function ListUserCard({ index, user }: ListUserCardProps) {
   return (
     <>
       <div className="flex items-center justify-between text-[16px]">
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-3 w-[70%]">
           <div>{index + 1}.</div>
-          <div>{user.name}</div>
+          <div className="truncate overflow-hidden whitespace-nowrap">{user.name}</div>
+
         </div>
         <div>
           <button className="bg-black text-white px-2 py-[2px] rounded-md">view</button>
