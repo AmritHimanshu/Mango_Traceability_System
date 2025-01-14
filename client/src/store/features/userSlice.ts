d5b83abe290d6257f9a5f5e6bfd7cd6995e0945e
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-14 22:23:46 +0530

    Fixed logout issue

--- client/src/store/features/userSlice.ts
+++ client/src/store/features/userSlice.ts
@@ -11,7 +11,7 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUserState: (state, action: PayloadAction<userSchema>) => {
+    setUserState: (state, action: PayloadAction<userSchema | null>) => {
       state.userState = action.payload;
     },
   },
