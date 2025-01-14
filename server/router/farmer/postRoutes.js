e96284f60fbeae3f4c754f30c9c51ad5a969a813
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-14 23:44:30 +0530

    Creating new-farm apis

--- server/router/farmer/postRoutes.js
+++ server/router/farmer/postRoutes.js
@@ -6,7 +6,17 @@ const Farmer = require('../../model/farmerSchema');
 router.post('/api/new-farm', async (req, res) => {
     try {
         const { farmName, cropName, coordinates } = req.body;
-        console.log(farmName, cropName, coordinates);
+
+        if (!farmName || !cropName || !coordinates) {
+            return res.status(422).json({ error: "Fill all the fields" });
+        }
+
+        if (coordinates.length < 3) {
+            return res.status(422).json({ error: "select minimum 3 coordinates" });
+        }
+
+        
+
         return res.status(201).json({ message: "OK" });
     } catch (error) {
         console.log("/api/new-farm: ", error);
