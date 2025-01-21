753779f27670bc86b899e75f781c8f1725b31cac
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-21 11:12:13 +0530

    Added 404 page

--- client/src/app/404/page.tsx
+++ client/src/app/404/page.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      <div>This is 404 page</div>
+    <div className='min-h-[calc(100vh-56px)] bg-gray-50 flex items-center justify-center'>
+      <div className='font-bold'>This is 404 page</div>
     </div>
   )
 }
