922d49cfc0b2143b761dbcd67d9be469f698cfec
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-25 09:13:15 +0530

    Working on the Downloading the farm's information in pdf form

--- client/src/app/farmer/certificate/page.tsx
+++ client/src/app/farmer/certificate/page.tsx
@@ -5,6 +5,8 @@ import { LoadingBarRef } from "react-top-loading-bar";
 import { useSearchParams } from "next/navigation";
 import { Farm } from "@/utils/Types/interfaces";
 import dynamic from "next/dynamic";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import { CERTIFICATE_FARM_DETAIL } from "@/utils/Apis/api";
 import ListFarmApplicationsData from "@/app/components/farmer/components/ListFarmApplicationsData";
 import CustomLoadingBar from "@/app/components/loadingBar/CustomLoadingBar";
@@ -19,6 +21,7 @@ function page() {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
   const loadingBarRef = useRef<LoadingBarRef>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const searchParams = useSearchParams();
   const farm_id = searchParams.get("farm_id");
@@ -60,6 +63,26 @@ function page() {
     }
   };
 
+  const downloadPDF = async () => {
+    if (!contentRef.current) return;
+
+    const element = contentRef.current;
+    const canvas = await html2canvas(element, { scale: 2 });
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "mm",
+      format: "a4",
+    });
+
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("Farm_Details.pdf");
+  };
+
   useEffect(() => {
     fetchFarmData();
   }, []);
@@ -69,217 +92,236 @@ function page() {
       <CustomLoadingBar ref={loadingBarRef} />
 
       {farmData && (
-        <div className="space-y-3 my-5">
-          <Map coordinates={farmData.geoFenceData} height="300px" />
+        <>
+          <div ref={contentRef} className="space-y-3 my-5">
+            <Map coordinates={farmData.geoFenceData} height="300px" />
 
-          <div className="bg-cardBackground p-3 rounded-md">
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Farmer Name: </div>
-              <div>{farmData.userId.name}</div>
-            </div>
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Farmer Email: </div>
-              <div>{farmData.userId.email}</div>
-            </div>
-          </div>
-
-          <div className="flex items-center space-x-3">
-            <div className="font-bold">Farm name:</div>
-            <div>{farmData.farm}</div>
-          </div>
-
-          <div className="flex items-center space-x-3">
-            <div className="font-bold">Crop name:</div>
-            <div>{farmData.crop}</div>
-          </div>
-
-          {farmData.ploughingDate && (
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Ploughing Date:</div>
-              <div>
-                {new Date(farmData.ploughingDate).toISOString().split("T")[0]}
+            <div className="bg-cardBackground p-3 rounded-md">
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Farmer Name: </div>
+                <div>{farmData.userId.name}</div>
+              </div>
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Farmer Email: </div>
+                <div>{farmData.userId.email}</div>
               </div>
             </div>
-          )}
 
-          {farmData.weedingDate && (
             <div className="flex items-center space-x-3">
-              <div className="font-bold">Weeding Date:</div>
-              <div>
-                {new Date(farmData.weedingDate).toISOString().split("T")[0]}
-              </div>
+              <div className="font-bold">Farm name:</div>
+              <div>{farmData.farm}</div>
             </div>
-          )}
 
-          {farmData.sowingDate && (
             <div className="flex items-center space-x-3">
-              <div className="font-bold">Sowing Date:</div>
-              <div>
-                {new Date(farmData.sowingDate).toISOString().split("T")[0]}
-              </div>
+              <div className="font-bold">Crop name:</div>
+              <div>{farmData.crop}</div>
             </div>
-          )}
 
-          {farmData.floweringDate && (
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Flowering Date:</div>
-              <div>
-                {new Date(farmData.floweringDate).toISOString().split("T")[0]}
-              </div>
-            </div>
-          )}
-
-          {farmData.pheromoneTrapDate && (
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Pheromone Trap Date:</div>
-              <div>
-                {
-                  new Date(farmData.pheromoneTrapDate)
-                    .toISOString()
-                    .split("T")[0]
-                }
-              </div>
-            </div>
-          )}
-
-          {farmData.lureChangeDate && (
-            <div className="flex items-center space-x-3">
-              <div className="font-bold">Lure Change Date:</div>
-              <div>
-                {new Date(farmData.lureChangeDate).toISOString().split("T")[0]}
-              </div>
-            </div>
-          )}
-
-          {(farmData.irrigationDates.artificial.length > 0 ||
-            farmData.irrigationDates.natural.length > 0) && (
-            <div className="mt-4">
-              <div className="font-bold mb-2">Irrigation Dates:</div>
-              <table className="w-full border-collapse border border-gray-300">
-                <thead>
-                  <tr className="bg-gray-100">
-                    {farmData.irrigationDates.artificial.length > 0 && (
-                      <th className="border border-gray-300 px-4 py-2">
-                        Artificial
-                      </th>
-                    )}
-                    {farmData.irrigationDates.natural.length > 0 && (
-                      <th className="border border-gray-300 px-4 py-2">
-                        Natural
-                      </th>
-                    )}
-                  </tr>
-                </thead>
-                <tbody>
-                  {(() => {
-                    const maxLength = Math.max(
-                      farmData.irrigationDates.artificial.length,
-                      farmData.irrigationDates.natural.length
-                    );
-
-                    return Array.from({ length: maxLength }).map((_, index) => (
-                      <tr
-                        key={index}
-                        className="hover:bg-gray-50 even:bg-gray-50 odd:bg-white"
-                      >
-                        {farmData.irrigationDates.artificial.length > 0 && (
-                          <td className="border border-gray-300 px-4 py-2">
-                            {farmData.irrigationDates.artificial[index]
-                              ? new Date(
-                                  farmData.irrigationDates.artificial[index]
-                                )
-                                  .toISOString()
-                                  .split("T")[0]
-                              : ""}
-                          </td>
-                        )}
-                        {farmData.irrigationDates.natural.length > 0 && (
-                          <td className="border border-gray-300 px-4 py-2">
-                            {farmData.irrigationDates.natural[index]
-                              ? new Date(
-                                  farmData.irrigationDates.natural[index]
-                                )
-                                  .toISOString()
-                                  .split("T")[0]
-                              : ""}
-                          </td>
-                        )}
-                      </tr>
-                    ));
-                  })()}
-                </tbody>
-              </table>
-            </div>
-          )}
-
-          {farmData.fertilizerApplications.length > 0 && (
-            <>
-              <div className="font-bold">Fertilizer Application:</div>
-              <ListFarmApplicationsData
-                data={farmData.fertilizerApplications}
-                columns={[
-                  { header: "Date", key: "date" },
-                  { header: "Volume (L)", key: "volume" },
-                ]}
-              />
-            </>
-          )}
-
-          {farmData.pesticideApplications.length > 0 && (
-            <>
-              <div className="font-bold">Pesticide Application:</div>
-              <ListFarmApplicationsData
-                data={farmData.pesticideApplications}
-                columns={[
-                  { header: "Date", key: "date" },
-                  { header: "Volume (L)", key: "volume" },
-                ]}
-              />
-            </>
-          )}
-
-          {farmData.bagging.length > 0 && (
-            <>
-              <div className="font-bold">Bagging:</div>
-              <ListFarmApplicationsData
-                data={farmData.bagging}
-                columns={[
-                  { header: "Date", key: "date" },
-                  { header: "Quantity", key: "quantity" },
-                ]}
-              />
-            </>
-          )}
-
-          {farmData.specialCare.length > 0 && (
-            <>
-              <div className="font-bold">Special care:</div>
-              <ListFarmApplicationsData
-                data={farmData.specialCare}
-                columns={[
-                  { header: "Date", key: "date" },
-                  { header: "Name", key: "name" },
-                ]}
-              />
-            </>
-          )}
-
-          {farmData.harvest && (
-            <>
-              <div className="font-bold">Harvest:</div>
-              <div className="flex space-x-5">
-                <div>Date:</div>
+            {farmData.ploughingDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Ploughing Date:</div>
                 <div>
-                  {new Date(farmData.harvest.date).toISOString().split("T")[0]}
+                  {new Date(farmData.ploughingDate).toISOString().split("T")[0]}
                 </div>
               </div>
-              <div className="flex space-x-5">
-                <div>Yield:</div>
-                <div>{farmData.harvest.yield}</div>
+            )}
+
+            {farmData.weedingDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Weeding Date:</div>
+                <div>
+                  {new Date(farmData.weedingDate).toISOString().split("T")[0]}
+                </div>
               </div>
-            </>
-          )}
-        </div>
+            )}
+
+            {farmData.sowingDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Sowing Date:</div>
+                <div>
+                  {new Date(farmData.sowingDate).toISOString().split("T")[0]}
+                </div>
+              </div>
+            )}
+
+            {farmData.floweringDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Flowering Date:</div>
+                <div>
+                  {new Date(farmData.floweringDate).toISOString().split("T")[0]}
+                </div>
+              </div>
+            )}
+
+            {farmData.pheromoneTrapDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Pheromone Trap Date:</div>
+                <div>
+                  {
+                    new Date(farmData.pheromoneTrapDate)
+                      .toISOString()
+                      .split("T")[0]
+                  }
+                </div>
+              </div>
+            )}
+
+            {farmData.lureChangeDate && (
+              <div className="flex items-center space-x-3">
+                <div className="font-bold">Lure Change Date:</div>
+                <div>
+                  {
+                    new Date(farmData.lureChangeDate)
+                      .toISOString()
+                      .split("T")[0]
+                  }
+                </div>
+              </div>
+            )}
+
+            {(farmData.irrigationDates.artificial.length > 0 ||
+              farmData.irrigationDates.natural.length > 0) && (
+              <div className="mt-4">
+                <div className="font-bold mb-2">Irrigation Dates:</div>
+                <table className="w-full border-collapse border border-gray-300">
+                  <thead>
+                    <tr className="bg-gray-100">
+                      {farmData.irrigationDates.artificial.length > 0 && (
+                        <th className="border border-gray-300 px-4 py-2">
+                          Artificial
+                        </th>
+                      )}
+                      {farmData.irrigationDates.natural.length > 0 && (
+                        <th className="border border-gray-300 px-4 py-2">
+                          Natural
+                        </th>
+                      )}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {(() => {
+                      const maxLength = Math.max(
+                        farmData.irrigationDates.artificial.length,
+                        farmData.irrigationDates.natural.length
+                      );
+
+                      return Array.from({ length: maxLength }).map(
+                        (_, index) => (
+                          <tr
+                            key={index}
+                            className="hover:bg-gray-50 even:bg-gray-50 odd:bg-white"
+                          >
+                            {farmData.irrigationDates.artificial.length > 0 && (
+                              <td className="border border-gray-300 px-4 py-2">
+                                {farmData.irrigationDates.artificial[index]
+                                  ? new Date(
+                                      farmData.irrigationDates.artificial[index]
+                                    )
+                                      .toISOString()
+                                      .split("T")[0]
+                                  : ""}
+                              </td>
+                            )}
+                            {farmData.irrigationDates.natural.length > 0 && (
+                              <td className="border border-gray-300 px-4 py-2">
+                                {farmData.irrigationDates.natural[index]
+                                  ? new Date(
+                                      farmData.irrigationDates.natural[index]
+                                    )
+                                      .toISOString()
+                                      .split("T")[0]
+                                  : ""}
+                              </td>
+                            )}
+                          </tr>
+                        )
+                      );
+                    })()}
+                  </tbody>
+                </table>
+              </div>
+            )}
+
+            {farmData.fertilizerApplications.length > 0 && (
+              <>
+                <div className="font-bold">Fertilizer Application:</div>
+                <ListFarmApplicationsData
+                  data={farmData.fertilizerApplications}
+                  columns={[
+                    { header: "Date", key: "date" },
+                    { header: "Volume (L)", key: "volume" },
+                  ]}
+                />
+              </>
+            )}
+
+            {farmData.pesticideApplications.length > 0 && (
+              <>
+                <div className="font-bold">Pesticide Application:</div>
+                <ListFarmApplicationsData
+                  data={farmData.pesticideApplications}
+                  columns={[
+                    { header: "Date", key: "date" },
+                    { header: "Volume (L)", key: "volume" },
+                  ]}
+                />
+              </>
+            )}
+
+            {farmData.bagging.length > 0 && (
+              <>
+                <div className="font-bold">Bagging:</div>
+                <ListFarmApplicationsData
+                  data={farmData.bagging}
+                  columns={[
+                    { header: "Date", key: "date" },
+                    { header: "Quantity", key: "quantity" },
+                  ]}
+                />
+              </>
+            )}
+
+            {farmData.specialCare.length > 0 && (
+              <>
+                <div className="font-bold">Special care:</div>
+                <ListFarmApplicationsData
+                  data={farmData.specialCare}
+                  columns={[
+                    { header: "Date", key: "date" },
+                    { header: "Name", key: "name" },
+                  ]}
+                />
+              </>
+            )}
+
+            {farmData.harvest && (
+              <>
+                <div className="font-bold">Harvest:</div>
+                <div className="flex space-x-5">
+                  <div>Date:</div>
+                  <div>
+                    {
+                      new Date(farmData.harvest.date)
+                        .toISOString()
+                        .split("T")[0]
+                    }
+                  </div>
+                </div>
+                <div className="flex space-x-5">
+                  <div>Yield:</div>
+                  <div>{farmData.harvest.yield}</div>
+                </div>
+              </>
+            )}
+          </div>
+          
+          <button
+            onClick={downloadPDF}
+            className="mt-5 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md"
+          >
+            Download PDF
+          </button>
+        </>
       )}
     </div>
   );
