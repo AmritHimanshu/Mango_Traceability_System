209174727fe35cf294c1427e4a3bfdffb124701c
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-01-26 18:15:09 +0530

    Working on the Downloading the farm's information in pdf form

--- client/src/app/farmer/certificate/page.tsx
+++ client/src/app/farmer/certificate/page.tsx
@@ -314,13 +314,12 @@ function page() {
               </>
             )}
           </div>
-          
-          <button
+          {/* <button
             onClick={downloadPDF}
             className="mt-5 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md"
           >
             Download PDF
-          </button>
+          </button> */}
         </>
       )}
     </div>
