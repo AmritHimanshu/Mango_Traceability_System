26d1b14d95d2b73d43b549b62c6b97242f187357
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-04 20:26:55 +0530

    Updating the UI

--- client/src/app/farmer/notifications/page.tsx
+++ client/src/app/farmer/notifications/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import React from 'react'
+import React, { useRef } from "react";
+import { LoadingBarRef } from "react-top-loading-bar";
+import CustomLoadingBar from "@/app/components/loadingBar/CustomLoadingBar";
+import Heading from "@/app/components/admin/Heading";
 
 function page() {
+
+  const loadingBarRef = useRef<LoadingBarRef>(null);
+
   return (
-    <div>
-      <div>This is notifications page</div>
+    <div className="p-5 w-full md:w-[calc(100vw-250px)] lg:w-[calc(100vw-300px)] xl:w-[calc(100vw-350px)] h-[calc(100vh-56px)] md:h-[calc(100vh-72px)] overflow-y-auto">
+      <CustomLoadingBar ref={loadingBarRef} />
+
+      <Heading text="NOTIFICATIONS" />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
