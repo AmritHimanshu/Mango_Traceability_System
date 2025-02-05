c7bcd3cce406cd9d8f3ad15e371c39cdf3cb00b5
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-05 16:55:53 +0530

    Modified the UI

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -313,16 +313,6 @@ function page() {
       return;
     }
 
-    if (!isEmailVerified) {
-      alert("Verify your email");
-      return;
-    }
-
-    if (!isPhoneVerified) {
-      alert("Verify your phone");
-      return;
-    }
-
     if (loadingBarRef.current) {
       loadingBarRef.current.continuousStart();
     }
