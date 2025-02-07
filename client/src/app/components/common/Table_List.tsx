473beeaa152aaf9434757736581c166b3e09921f
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-07 21:51:34 +0530

    Updating the UI

---
+++ client/src/app/components/common/Table_List.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { User } from "@/utils/Types/interfaces";
+import { useRouter } from "next/navigation";
+
+function Table_List({ users, url }: { users: User[]; url: string }) {
+  const router = useRouter();
+
+  const handleOnView = async (farmer_id: string) => {
+    router.push(`${url}/${farmer_id}`);
+  };
+
+  return (
+    <div className="w-full overflow-x-auto">
+      <table className="w-full text-[10px] md:text-[13px] lg:text-[16px] border-2 table-fixed">
+        <thead>
+          <tr className="text-start font-bold bg-gray-100">
+            <td className="px-2 py-4 border-y-2">Name</td>
+            <td className="px-2 py-4 border-y-2">Email</td>
+            <td className="px-2 py-4 border-y-2">Phone</td>
+            <td className="px-2 py-4 border-y-2">Joined on</td>
+            <td className="px-2 py-4 border-y-2"></td>
+          </tr>
+        </thead>
+
+        <tbody className="text-[9px] md:text-[12px] lg:text-[16px]">
+          {users.map((user, index) => (
+            <tr key={index} className="text-start">
+              <td
+                className="px-2 py-4 border-y-2 w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                title={user.name}
+              >
+                {user.name}
+              </td>
+              <td 
+                className="px-2 py-4 border-y-2 w-[150px] min-w-[120px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                title={user.email}
+              >
+                {user.email}
+              </td>
+              <td
+                className="px-2 py-4 border-y-2 w-[100px] min-w-[80px] lg:min-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap"
+                title={String(user.phone)}
+              >
+                {user.phone}
+              </td>
+              <td className="px-2 py-4 border-y-2 w-[120px] min-w-[100px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap">
+                {new Date(user.createdAt).toLocaleString("en-IN", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                })}
+              </td>
+              <td className="px-2 border-y-2">
+                <button
+                  className="w-full bg-green-600 bg-opacity-90 hover:bg-opacity-100 text-white py-2 rounded-sm duration-200"
+                  onClick={() => handleOnView(user._id)}
+                >
+                  View
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table_List;
