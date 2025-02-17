0b63817cc683cba3c14ecce28c9a2f8d983518e4
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-17 21:23:18 +0530

    Working on farm edit functionality on Admin side

--- client/src/app/admin/farmer/farm/edit/page.tsx
+++ client/src/app/admin/farmer/farm/edit/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { LoadingBarRef } from "react-top-loading-bar";
 import { Farm } from "@/utils/Types/interfaces";
 import CustomLoadingBar from "@/app/components/loadingBar/CustomLoadingBar";
 import Heading from "@/app/components/common/Heading";
-import { ADMIN_FARM } from "@/utils/Paths/paths";
+import { ADMIN_FARM, LOGIN, NOT_FOUND } from "@/utils/Paths/paths";
+import { ADMIN_FETCH_FARMER_FARM_DATA } from "@/utils/Apis/api";
 
 function page() {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -18,13 +19,70 @@ function page() {
   const farm_id = searchParams.get("farm_id");
 
   const [farmData, setFarmData] = useState<Farm>();
+  const [message, setMessage] = useState({ text: "", type: "" });
 
   const handleOnCancel = () => {
     router.push(`${ADMIN_FARM}?farm_id=${farm_id}`);
   };
 
+  const fetchFarmData = async () => {
+    if (loadingBarRef.current) {
+      loadingBarRef.current.continuousStart();
+    }
+
+    try {
+      const res = await fetch(
+        `${BASE_URL}/${ADMIN_FETCH_FARMER_FARM_DATA}/${farm_id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      if (res.status === 404) {
+        setMessage({ text: data.error, type: "error" });
+        router.push(NOT_FOUND);
+        const error = new Error(data.error);
+        throw error;
+      }
+
+      if (res.status !== 201 && res.status !== 500) {
+        setMessage({ text: data.error, type: "error" });
+        router.push(LOGIN);
+        const error = new Error(data.error);
+        throw error;
+      }
+
+      if (res.status === 500) {
+        setMessage({ text: data.error, type: "error" });
+        const error = new Error(data.error);
+        throw error;
+      }
+
+      setFarmData(data);
+    } catch (error) {}
+
+    setTimeout(() => {
+      setMessage({ text: "", type: "" });
+    }, 2000);
+
+    if (loadingBarRef.current) {
+      loadingBarRef.current.complete();
+    }
+  };
+
+  useEffect(() => {
+    fetchFarmData();
+  }, []);
+
   return (
-    <div>
+    <div className="page-main-div relative">
       <CustomLoadingBar ref={loadingBarRef} />
 
       {farmData && <Heading text={`${farmData.farm} (Edit)`} />}
