59fbef02ace7ad90765b3a062df0cff8c0c9c361
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-17 21:03:47 +0530

    Added back button

--- client/src/app/components/common/Heading.tsx
+++ client/src/app/components/common/Heading.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useRouter } from "next/navigation";
 
 function Heading({ text }: { text: string }) {
+  const router = useRouter();
+
   return (
     <>
-      <div className="pb-2 md:pb-4 lg:pb-3 font-bold md:text-[16px] lg:text-[20px]">
-        {text}
+      <div className="pb-2 md:pb-4 lg:pb-3 font-bold md:text-[16px] lg:text-[20px] space-x-2">
+        <ArrowBackIcon
+          style={{ cursor: "pointer" }}
+          onClick={() => router.back()}
+        />{" "}
+        <span>{text}</span>
       </div>
       <hr className="border-[1px] border-gray-200" />
     </>
