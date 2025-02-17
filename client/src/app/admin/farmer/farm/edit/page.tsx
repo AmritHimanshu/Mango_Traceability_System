67455934ef74270743d1f731462a47e15bd9cb73
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-17 17:43:05 +0530

    Working on edit functionality on admin side

--- client/src/app/admin/farmer/farm/edit/page.tsx
+++ client/src/app/admin/farmer/farm/edit/page.tsx
@@ -1,13 +1,44 @@
 "use client";
 
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { LoadingBarRef } from "react-top-loading-bar";
+import { Farm } from "@/utils/Types/interfaces";
+import CustomLoadingBar from "@/app/components/loadingBar/CustomLoadingBar";
+import Heading from "@/app/components/common/Heading";
+import { ADMIN_FARM } from "@/utils/Paths/paths";
 
 function page() {
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const loadingBarRef = useRef<LoadingBarRef>(null);
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const farm_id = searchParams.get("farm_id");
+
+  const [farmData, setFarmData] = useState<Farm>();
+
+  const handleOnCancel = () => {
+    router.push(`${ADMIN_FARM}?farm_id=${farm_id}`);
+  };
+
   return (
     <div>
-      This is edit page
+      <CustomLoadingBar ref={loadingBarRef} />
+
+      {farmData && <Heading text={`${farmData.farm} (Edit)`} />}
+
+      <div className="text-end my-7">
+        <button
+          onClick={handleOnCancel}
+          className="bg-red-600 text-white hover:bg-red-100 hover:text-red-600 duration-200 rounded-sm px-2 py-1 text-[11px] md:text-lg"
+        >
+          Cancel
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
