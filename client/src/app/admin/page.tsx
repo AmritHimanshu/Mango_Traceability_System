1118cfc68088a847b364642479b83937b32c3090
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-18 22:31:31 +0530

    Working on UI

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -204,7 +204,7 @@ function page() {
 
       <Heading text="OVERVIEW" />
 
-      <div className="pt-5 grid grid-cols-2 gap-5">
+      <div className="pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center">
         <HomeCard
           title="Total number of verified managers"
           description=""
--- client/src/app/components/admin/HomeCard.tsx
+++ client/src/app/components/admin/HomeCard.tsx
@@ -15,10 +15,10 @@ function HomeCard({ title, description, count, textColor }: HomeCardProps) {
     const textColorClass = allowedColors[textColor] || "text-black";
 
   return (
-    <div className={`p-3 md:p-5 text-[13px] md:text-[16px] text-center font-bold bg-cardBackground rounded-md shadow-md ${textColorClass}`}>
-      <p>{title}</p>
+    <div className={`p-3 md:p-5 text-[13px] md:text-[16px] text-center font-bold bg-cardBackground w-[250px] xl:w-[250px] 2xl:w-[300px] h-[250px] xl:h-[250px] 2xl:h-[300px] rounded-full shadow-md ${textColorClass}`}>
+      {/* <p>{title}</p>
       <p className="text-[12px] font-normal">{description}</p>
-      <p className="text-[16px] md:text-[20px]">{count}</p>
+      <p className="text-[16px] md:text-[20px]">{count}</p> */}
     </div>
   );
 }
