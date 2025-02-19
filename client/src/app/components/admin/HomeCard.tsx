908835ddf26417a8fb60a91cb1ca36fc4228923b
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-19 11:37:01 +0530

    Working on the UI

--- client/src/app/components/admin/HomeCard.tsx
+++ client/src/app/components/admin/HomeCard.tsx
@@ -15,7 +15,7 @@ function HomeCard({ title, description, count, textColor }: HomeCardProps) {
     const textColorClass = allowedColors[textColor] || "text-black";
 
   return (
-    <div className={`p-3 md:p-5 text-[13px] md:text-[16px] font-bold bg-cardBackground w-[250px] xl:w-[250px] 2xl:w-[300px] h-[250px] xl:h-[250px] 2xl:h-[300px] rounded-full shadow-md flex flex-col items-center justify-center ${textColorClass}`}>
+    <div className={`p-3 md:p-5 space-y-2 text-[13px] md:text-[16px] text-center font-bold bg-gray0 shadow-xl w-[250px] xl:w-[250px] 2xl:w-[300px] h-[250px] xl:h-[250px] 2xl:h-[300px] rounded-full flex flex-col items-center justify-center ${textColorClass} hover:bg-sky-100 hover:bg-gradient-to-t hover:from-sky-100 hover:to-white duration-1000 overflow-hidden`}>
       <p>{title}</p>
       <p className="text-[12px] font-normal">{description}</p>
       <p className="text-[16px] md:text-[20px]">{count}</p>
