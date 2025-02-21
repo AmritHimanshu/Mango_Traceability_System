3e1abd35238014d9c605c9a26a07292d376be88b
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-21 22:34:15 +0530

    Working on UI

--- client/src/app/components/admin/ListFarmTable.tsx
+++ client/src/app/components/admin/ListFarmTable.tsx
@@ -2,19 +2,18 @@
 
 import React from "react";
 import { ListFarmTableProps } from "@/utils/Types/interfaces";
-import { MDBBtn } from "mdb-react-ui-kit";
 
 function ListFarmTable({ farms, handleClick }: ListFarmTableProps) {
   return (
-    <div className="w-full max-w-[1100px] m-auto overflow-x-auto rounded-lg overflow-hidden shadow-2xl">
+    <div className="w-full overflow-x-auto rounded-lg shadow-2xl">
       <table className="w-full text-[10px] md:text-[13px] lg:text-[16px] table-fixed">
         <thead>
-          <tr className="text-start font-bold bg-sky-600 text-white">
-            <td className="px-2 py-4 border-y-2">ID</td>
-            <td className="px-2 py-4 border-y-2">Farm Name</td>
-            <td className="px-2 py-4 border-y-2">Crop Name</td>
-            <td className="px-2 py-4 border-y-2">Created on</td>
-            <td className="px-2 py-4 border-y-2"></td>
+          <tr className="font-bold bg-primarycColor bg-opacity-80 text-white">
+            <td className="px-4 py-3 text-left">ID</td>
+            <td className="px-4 py-3 text-left">Farm Name</td>
+            <td className="px-4 py-3 text-left">Crop Name</td>
+            <td className="px-4 py-3 text-left">Created on</td>
+            <td className="px-4 py-3 text-left"></td>
           </tr>
         </thead>
 
@@ -22,27 +21,27 @@ function ListFarmTable({ farms, handleClick }: ListFarmTableProps) {
           {farms.map((farm, index) => (
             <tr
               key={index}
-              className="text-start text-black bg-sky-100 border-b-[1px] border-black last:border-b-0"
+              className="text-black bg-secondaryColor odd:bg-opacity-30 border-b-[1px] border-black last:border-b-0"
             >
               <td
-                className="px-2 py-4 w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                className="px-4 py-3 align-middle w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
                 title={farm.uniqueID}
               >
                 {farm.uniqueID}
               </td>
               <td
-                className="px-2 py-4 w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                className="px-4 py-3 align-middle w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
                 title={farm.farm}
               >
                 {farm.farm}
               </td>
               <td
-                className="px-2 py-4 w-[150px] min-w-[120px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                className="px-4 py-3 align-middle w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
                 title={farm.crop}
               >
                 {farm.crop}
               </td>
-              <td className="px-2 py-4 w-[120px] min-w-[100px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap">
+              <td className="px-4 py-3 align-middle w-[100px] min-w-[80px] lg:min-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap">
                 {new Date(farm.createdAt).toLocaleString("en-IN", {
                   day: "2-digit",
                   month: "2-digit",
@@ -53,12 +52,12 @@ function ListFarmTable({ farms, handleClick }: ListFarmTableProps) {
                 })}
               </td>
               <td className="px-2 text-end">
-                <MDBBtn
-                  className="!w-[30px] md:!w-[50px] lg:!w-[100px] !text-[13px]"
+                <button
+                  className="!w-[30px] md:!w-[50px] lg:!w-[100px] !text-[9px] md:!text-[12px] lg:!text-[16px] py-[3px] lg:py-[7px] bg-green-900 bg-opacity-80 text-white font-bold rounded-[5px] hover:shadow-md hover:bg-opacity-85 duration-200"
                   onClick={() => handleClick(farm._id)}
                 >
                   View
-                </MDBBtn>
+                </button>
               </td>
             </tr>
           ))}
