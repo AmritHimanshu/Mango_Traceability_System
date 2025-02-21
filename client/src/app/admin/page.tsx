97ba74ef8f0b7200255a00e7dabbb8f34b7c5b99
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-21 21:11:36 +0530

    Working on UI

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -204,7 +204,7 @@ function page() {
 
       <Heading text="OVERVIEW" />
 
-      <div className="pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 place-items-center">
+      <div className="pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center">
         <HomeCard
           title="Total number of verified managers"
           description=""
@@ -239,7 +239,7 @@ function page() {
           <div className="w-full overflow-x-auto rounded-lg overflow-hidden shadow-2xl">
             <table className="w-full text-[10px] md:text-[13px] lg:text-[16px] table-fixed">
               <thead>
-                <tr className="text-start font-bold bg-sky-600 text-white">
+                <tr className="text-start font-bold bg-sky-600 bg-opacity-95 text-white">
                   <td className="px-2 py-4">Name</td>
                   <td className="px-2 py-4">Email</td>
                   <td className="px-2 py-4">Phone</td>
--- client/src/app/components/admin/HomeCard.tsx
+++ client/src/app/components/admin/HomeCard.tsx
@@ -15,12 +15,12 @@ function HomeCard({ title, description, count, bgColor }: HomeCardProps) {
   const bgColorClass = allowedColors[bgColor] || "bg-white";
 
   return (
-    <div
-      className={`p-3 md:p-5 space-y-1 lg:space-y-2 text-[13px] md:text-[16px] text-center font-bold bg-gray0 shadow-xl w-[250px] xl:w-[250px] 2xl:w-[300px] h-[250px] xl:h-[250px] 2xl:h-[300px] rounded-full flex flex-col items-center justify-center text-white ${bgColorClass} hoverbg-sky-100 hoverbg-gradient-to-t hoverfrom-sky-100 hoverto-white duration-1000 overflow-hidden relative`}>
-      <p>{title}</p>
-      <p className="text-[12px] font-normal">{description}</p>
-      <p className="text-[16px] md:text-[20px]">{count}</p>
-    </div>
+    <>
+        <div className="rounded-lg overflow-hidden min-w-[300px] text-center hover:shadow-sm duration-150">
+          <div className="p-2 bg-sky-600 text-white font-bold text-sm sm:text-base">{title}</div>
+          <div className="py-3 font-bold bg-sky-50 text-black text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">{count}</div>
+        </div>
+    </>
   );
 }
 
