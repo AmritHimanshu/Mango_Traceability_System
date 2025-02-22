47847c34edc2b77a140472625425ee0c3db26b5b
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-02-22 08:13:03 +0530

    Working on UI

--- client/src/app/components/admin/HomeCard.tsx
+++ client/src/app/components/admin/HomeCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { HomeCardProps } from "@/utils/Types/interfaces";
 
 const allowedColors: Record<string, string> = {
@@ -13,14 +13,35 @@ const allowedColors: Record<string, string> = {
 function HomeCard({ title, description, count, bgColor }: HomeCardProps) {
   const bgColorClass = allowedColors[bgColor] || "bg-white";
 
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <div className="rounded-lg overflow-hidden min-w-[300px] text-center shadow-md duration-150">
+    <div
+      className="rounded-lg overflow-hidden min-w-[300px] text-center shadow-md duration-150 relative"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <div
+        className={`w-full flex items-center justify-center absolute bottom-0 duration-300 bg-neutral-800 bg-opacity-65 ${
+          isHover ? "h-full" : "h-0"
+        }`}
+      >
+        {isHover && (
+          <button className={`!w-[30px] md:!w-[50px] lg:!w-[100px] !text-[9px] md:!text-[12px] lg:!text-[16px] py-[3px] lg:py-[7px] text-white font-bold rounded-[5px] hover:shadow-md hover:bg-opacity-85 duration-200 ${bgColorClass}`}>
+            view
+          </button>
+        )}
+      </div>
+
       <div
         className={`p-2 xl:!p-5 bg-opacity-90 text-white font-bold text-sm sm:text-base ${bgColorClass}`}
       >
         {title}
       </div>
-      <div className={`py-3 xl:py-5 font-bold text-black text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ${bgColorClass} bg-opacity-10`}>
+
+      <div
+        className={`py-3 xl:py-5 font-bold text-black text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ${bgColorClass} bg-opacity-10`}
+      >
         {count}
       </div>
     </div>
