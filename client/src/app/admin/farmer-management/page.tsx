b8acf3ba327f1169effaa479145939792d2d526e
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-03-02 14:51:59 +0530

    Working on UI

--- client/src/app/admin/farmer-management/page.tsx
+++ client/src/app/admin/farmer-management/page.tsx
@@ -2,13 +2,13 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { LoadingBarRef } from "react-top-loading-bar";
 import CustomLoadingBar from "@/app/components/common/loadingBar/CustomLoadingBar";
 import { User } from "@/utils/Types/interfaces";
 import { FARMER, LOGIN } from "@/utils/Paths/paths";
 import { ADMIN_FARMER_MANAGEMENT } from "@/utils/Apis/api";
 import Message from "@/app/components/common/Message";
-import Heading from "@/app/components/common/Heading";
 import Table_List from "@/app/components/admin/Table_List";
 
 function page() {
@@ -95,19 +95,39 @@ function page() {
   return (
     <div className="page-main-div">
       <CustomLoadingBar ref={loadingBarRef} />
- 
+
       {message.text && message.type && (
         <Message text={message.text} type={message.type} />
       )}
 
-      <Heading text="FARMER MANAGEMENT" />
+      <div className="h-[500px] md:h-[600px] xl:h-[400px] relative">
+        <Image
+          src="/assets/farmers_image.jpg"
+          alt="Manager"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+        <div className="p-3 md:p-5 absolute top-0 w-full h-full bg-neutral-950 bg-opacity-50 flex items-center justify-center">
+          <div className="w-[80%] m-auto">
+            <div className="text-[30px] md:text-[50px] font-bold text-white">
+              Farmer Management
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div className="mt-5">
-        {farmers.length > 0 ? (
-          <Table_List users={farmers} url={FARMER}/>
-        ) : (
-          <div className="text-center text-gray-500">No records found!</div>
-        )}
+      <div className="my-5">
+        <div className="max-w-[80%] m-auto space-y-5">
+          <div className="text-center font-bold text-base md:text-lg lg:text-xl xl:text-2xl text-black">
+            Farmers
+          </div>
+          {farmers.length > 0 ? (
+            <Table_List users={farmers} url={FARMER} />
+          ) : (
+            <div className="text-center text-gray-500">No records found!</div>
+          )}
+        </div>
       </div>
     </div>
   );
