095cdc000f4d952f7aaa086311136dfd25c02608
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-03-06 11:30:22 +0530

    Working on PWA

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import nextPwa from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // reactStrictMode: false
-};
+const nextConfig = nextPwa({
+  dest: "public", // Service Worker will be generated in "public" folder
+  disable: process.env.NODE_ENV === "development", // Disable PWA in dev mode
+})({
+  // reactStrictMode: true,
+});
 
 export default nextConfig;
