82a0ea8f357fc919bf9625d44db513b7ab070e1d
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-03-21 15:50:02 +0530

    Working on UI

--- client/src/app/components/common/Header/Login_Header.tsx
+++ client/src/app/components/common/Header/Login_Header.tsx
@@ -1,20 +1,35 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 
 function Login_Header() {
   return (
-    <div className='sticky w-full top-0 z-[9999] bg-white transition-all duration-300 flex items-center justify-between'>
-      <div className='py-3 px-5'>
-        <Image src="/assets/cdac-logo.png" alt="Logo" width={50} height={50} className='bg-white p-1 rounded-sm'/>
+    <div className="sticky w-full top-0 z-[9999] bg-white transition-all duration-300 flex items-center justify-between">
+      <div className="py-3 px-5">
+        <Image
+          src="/assets/cdac-logo.png"
+          alt="Logo"
+          width={50}
+          height={50}
+          className="bg-white p-1 rounded-sm"
+        />
       </div>
-      <div className='font-bold text-customOrange'>Making Maldha Aam Accessible to every Indian</div>
-      <div className='py-3 px-5'></div>
+      <div className="text-center">
+        <div className="font-bold text-customOrange text-xs md:text-sm lg:text-xl">
+          Making Maldha Aam Accessible to every Indian
+        </div>
+        <div className="text-xs text-black hidden lg:block">
+          we are dedicated to deliver fresh and natural mangoes which are grown
+          chemical residue free to your door step. We provide all variety of
+          mangoes to our customers.
+        </div>
+      </div>
+      <div className="py-3 px-5"></div>
     </div>
-  )
+  );
 }
 
-export default Login_Header
+export default Login_Header;
 
 // bg-[url(/assets/login-bg.jpg)] bg-cover bg-bottom bg-no-repeat
