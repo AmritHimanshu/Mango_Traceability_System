43e12a637b08fe49a5afe6cec7255d08442debcc
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-04-25 08:12:01 +0530

    Organized the socket.io connection

--- server/notification/weatherAlerts.js
+++ server/notification/weatherAlerts.js
@@ -105,7 +105,6 @@ const runWeatherAlertJob = async () => {
 
 const sendInstantWeatherAlertToUser = async (userId) => {
     try {
-        console.log("hi")
         const farms = await Farmer.find({ userUniqueId: userId });
 
         const notifiedBlocks = new Set();
