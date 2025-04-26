0c04a31f8b6b9838aaca5cb360b8aea5ac1ff3bd
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-04-26 16:28:35 +0530

    Modifying UI

--- client/src/app/components/admin/HomeCard.tsx
+++ client/src/app/components/admin/HomeCard.tsx
@@ -22,55 +22,9 @@ function HomeCard({
 }: HomeCardProps) {
   const textColorClass = allowedColors[textColor] || "text-black";
 
-  const [countNum, setCountNum] = useState(0);
-  const duration = 2000;
-
-  const cardRef = useRef<HTMLDivElement | null>(null);
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const [entry] = entries;
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-        }
-      },
-      { threshold: 0.5 }
-    );
-
-    if (cardRef.current) {
-      observer.observe(cardRef.current);
-    }
-
-    return () => {
-      if (cardRef.current) {
-        observer.unobserve(cardRef.current);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!isVisible || countNum > 0) return;
-
-    let startValue = 0;
-    const interval = Math.floor(duration / (count - 0));
-
-    const counter = setInterval(() => {
-      startValue += 1;
-      setCountNum(startValue);
-      if (startValue >= count) {
-        clearInterval(counter);
-      }
-    }, interval);
-
-    return () => {
-      clearInterval(counter);
-    };
-  }, [isVisible, count]);
 
   return (
-    <div ref={cardRef} className="w-[100%] flex items-center justify-between border-[1px] border-gray-600 rounded-md p-2">
+    <div className="w-[100%] flex items-center justify-between border-[1px] border-gray-600 rounded-md p-2">
       <div className="w-[30%] text-black text-center space-y-2">
         <div>
           {description === "pending_requests" && (
@@ -91,7 +45,7 @@ function HomeCard({
         <div
           className={`md:text-lg lg:text-[25px] xl:text-[30px] font-bold ${textColorClass}`}
         >
-          {countNum}
+          {count}
         </div>
       </div>
 
