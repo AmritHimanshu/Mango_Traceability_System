deacb246b27557c3a3196da466cb592daa6fe6a8
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-04-26 14:20:37 +0530

    Modifying UI

--- client/src/app/farmer/page.tsx
+++ client/src/app/farmer/page.tsx
@@ -10,7 +10,6 @@ import { LOGIN } from "@/utils/Paths/paths";
 import { FewFarmList } from "@/utils/Types/interfaces";
 import HomeCard from "../components/farmer/HomeCard";
 import Message from "../components/common/Message";
-import HomeAboutUs from "../components/common/HomeAboutUs";
 
 function page() {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -119,10 +118,6 @@ function page() {
           </div>
         </div>
       </div>
-
-      {/* <div className="my-5 p-3 md:p-5">
-        <HomeAboutUs />
-      </div> */}
 
       <div>
         {farmList.length > 0 && (
